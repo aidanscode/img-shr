1f79dc1f3a46a1085127fefa5fa0ebbf6e651026
Aidan Murphey <amurphey7@gmail.com>
2024-08-22 13:19:08 -0400

    File upload validation

--- app/handler/upload.go
+++ app/handler/upload.go
@@ -1,11 +1,18 @@
 package handler
 
 import (
+	"errors"
+	"io"
+	"mime/multipart"
 	"net/http"
 	"strings"
 
 	"github.com/labstack/echo/v4"
 )
+
+var allowedImageTypes = []string{
+	"image/jpeg", "image/png", "image/gif",
+}
 
 func (h *Handler) UploadForm(c echo.Context) error {
 	return c.Render(http.StatusOK, "upload", UploadFormData{Title: "", Error: ""})
@@ -16,7 +23,38 @@ func (h *Handler) Upload(c echo.Context) error {
 	if len(title) == 0 {
 		return c.Render(http.StatusOK, "upload.upload-form", UploadFormData{Title: title, Error: "Missing title"})
 	}
-	return c.Render(http.StatusOK, "upload", nil)
+
+	file, err := c.FormFile("image")
+	if err != nil {
+		return c.Render(http.StatusOK, "upload.upload-form", UploadFormData{Title: title, Error: "Missing image"})
+	}
+	src, err := file.Open()
+	if err != nil {
+		return c.Render(http.StatusOK, "upload.upload-form", UploadFormData{Title: title, Error: "Invalid image uploaded"})
+	}
+	defer src.Close()
+
+	if err := validateMimeType(src); err != nil {
+		return c.Render(http.StatusOK, "upload.upload-form", UploadFormData{Title: title, Error: err.Error()})
+	}
+
+	return c.Render(http.StatusOK, "upload.upload-form", UploadFormData{Title: title, Error: "Okay!"})
+}
+
+func validateMimeType(f multipart.File) error {
+	contents, err := io.ReadAll(f)
+	if err != nil {
+		return errors.New("invalid image uploaded")
+	}
+
+	mimeType := http.DetectContentType(contents)
+	for _, allowedMimeType := range allowedImageTypes {
+		if allowedMimeType == mimeType {
+			return nil
+		}
+	}
+
+	return errors.New("invalid file type. Must be one of: " + strings.Join(allowedImageTypes, ", "))
 }
 
 type UploadFormData struct {
